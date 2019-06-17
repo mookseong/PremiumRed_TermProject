eb9c255da5f918b7278c65d924b09d2d4077476a
SeungHyeun-Ka <rktmdgus12@naver.com>
2019-06-18 00:25:17 +0900

    mp3폴대 내에 있는 파일들 순서대로 재생구현 및 JLayer적용

--- src/MP3Demo.java
+++ src/MP3Demo.java
@@ -1,35 +1,30 @@
-import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.GridLayout;
-import java.awt.Point;
-import java.awt.Toolkit;
-import java.util.List;
-import java.util.Stack;
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.io.File;
+import java.io.InputStream;
 import java.io.FileInputStream;
 import javazoom.jl.player.Player;
 
-import javax.swing.*;
 
-public class MP3Demo {
+class MP3Demo {
 
-private JList jl;
-private JFileChooser fileChooser = new JFileChooser();
-private JMenuItem itemDel;
-private JMenuItem imtePlay;
-
-
-
-
+    private static final InputStream FileInputStream = null;
 
     public static void main(String[] args) {
-        try {
-            FileInputStream fis = new FileInputStream(".mp3");
-            Player playMp3 = new Player(fis);
-            playMp3.play();
-        } catch (Exception e) {
+
+        try{
+            File mtDir = new File("./mp3");
+            File[] contents =  mtDir.listFiles();
+            for (int i=0; i<contents.length; i++) {
+                if (contents[i].isFile()) {
+                    Player playMp3 = new Player(new FileInputStream(contents[i]));
+                    playMp3.play();
+                }
+            }
+
+        }
+        catch(Exception e){
             System.out.println(e);
         }
+
     }
+
 }
