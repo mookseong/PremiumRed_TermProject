7d9bd7c2857dc5901e975805ee25962eb92865d5
LimSeongmuk <mookseong>
2019-06-19 14:44:29 +0900

    버튼과 이미지 변경

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,16 +1,13 @@
 package GUI;
 
-import Data.XML_Reading;
-import Data.XML_wirte;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
-import java.awt.event.ActionEvent;
-import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,13 +15,29 @@ public class Controller implements Initializable {
     @FXML public ImageView Play_Btn;
     @FXML private Label Title;
 
+    private boolean PlayImg = false;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Title.setText("음악이 재생해주시기 바랍니다.");
-        Play_Btn.setOnMouseClicked((EventHandler<Event>) evnet -> handlePlay());
+        Title.setText("음악을 재생해주시기 바랍니다.");
+        Play_Btn.setOnMouseClicked((EventHandler<Event>) Evnet -> handlePlay());
     }
 
     private void handlePlay(){
-        System.out.println("이미지를 클릭하셨습니다");
+        if (PlayImg) {
+            Play_Btn.setImage(new Image(getClass().getResourceAsStream("../Img/Pause.png")));
+            System.out.println("일시 정지합니다.");
+            PlayImg = false;
+        }
+        else{
+            YoutubeDownLoad();
+            Play_Btn.setImage(new Image(getClass().getResourceAsStream("../Img/Play.png")));
+            System.out.println("노래를 재생합니다.");
+            PlayImg = true;
+        }
+    }
+
+    private void YoutubeDownLoad(){
+
     }
 }
